02b9021b5f89f28ca78e526baa7165afb32d13a6
A4AMEEN <alameenmail07@gmail.com>
2024-10-17 18:58:40 +0530

    Secondddddd commit

--- src/providers/database.ts
+++ src/providers/database.ts
@@ -4,9 +4,17 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const connectDB = async () => {
-  const uri = process.env.DATABASE_URI as string; // Use the environment variable for the database URI
-  
-  // Create a MongoClient with a MongoClientOptions object to set the Stable API version
+  const uri = process.env.DATABASE_URI as string;
+
+  // Log the URI for debugging
+  console.log('Connecting to MongoDB using URI:', uri); 
+
+  // Ensure that uri is defined
+  if (!uri) {
+    console.error('DATABASE_URI is undefined. Please check your .env file.');
+    process.exit(1);
+  }
+
   const client = new MongoClient(uri, {
     serverApi: {
       version: ServerApiVersion.v1,
@@ -16,16 +24,13 @@ const connectDB = async () => {
   });
 
   try {
-    // Connect the client to the server
     await client.connect();
-    // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } catch (err) {
     console.error('Failed to connect to MongoDB:', err);
     process.exit(1);
   } finally {
-    // Ensures that the client will close when you finish/error
     await client.close();
   }
 };
