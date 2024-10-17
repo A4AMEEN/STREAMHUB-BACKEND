529c8e72085a2505cb79f1883afc3921d7de0f84
A4AMEEN <alameenmail07@gmail.com>
2024-10-17 18:54:52 +0530

    Seconddddd commit

--- src/providers/database.ts
+++ src/providers/database.ts
@@ -1,15 +1,32 @@
-import mongoose from 'mongoose';
+import { MongoClient, ServerApiVersion } from 'mongodb';
 import dotenv from 'dotenv';
 
 dotenv.config();
 
 const connectDB = async () => {
+  const uri = process.env.DATABASE_URI as string; // Use the environment variable for the database URI
+  
+  // Create a MongoClient with a MongoClientOptions object to set the Stable API version
+  const client = new MongoClient(uri, {
+    serverApi: {
+      version: ServerApiVersion.v1,
+      strict: true,
+      deprecationErrors: true,
+    }
+  });
+
   try {
-    await mongoose.connect(process.env.DATABASE_URI as string);
-    console.log('MongoDB connected...');
+    // Connect the client to the server
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } catch (err) {
-    console.error('Failed to connect to MongoDB:');
+    console.error('Failed to connect to MongoDB:', err);
     process.exit(1);
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
   }
 };
 
